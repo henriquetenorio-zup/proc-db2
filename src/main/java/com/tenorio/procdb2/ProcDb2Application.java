3a0eec17affa721b9ea8638bb4982741fe1827b9
Michael H. Tenorio <rike.tenorio@gmail.com>
2020-09-25 15:10:53 -0400

    DB2 TESTE PROC

---
+++ src/main/java/com/tenorio/procdb2/ProcDb2Application.java
@@ -0,0 +1,13 @@
+package com.tenorio.procdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcDb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenorio/procdb2/ProcDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenorio.procdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
